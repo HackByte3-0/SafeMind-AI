8becacede5c77b966f85cda83b465617d17ba05b
Aryan <aryanyk562@gmail.com>
2025-04-04 23:08:45 +0530

    Added new functions to handle pages

--- app/views.py
+++ app/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+
+def about(request):
+    return render(request, 'app/about.html')
+
+def contact(request):
+    return render(request, 'app/contact.html')
+
+def dashboard(request):
+    return render(request, 'app/dashboard.html')
+
+def how_to_use(request):
+    return render(request, 'app/how_to_use.html')
+
+def book_consultation(request):
+    return render(request, 'app/book_consultation.html')
