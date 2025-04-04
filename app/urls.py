bcbc31e5485471cc12be1d898df4b461c18a38d4
Aryan <aryanyk562@gmail.com>
2025-04-05 02:05:23 +0530

    Added phq-9 form

--- app/urls.py
+++ app/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('dashboard/',dashboard,name='dashboard'),
     path('how-to-use/',how_to_use,name='how-to-use'),
     path('book-consultation/',book_consultation,name='book_consultation'),
+    path('video_feed/', video_feed, name='video_feed'),
+    path("assesment/", phq9_view, name="phq9"),
     
 ]
